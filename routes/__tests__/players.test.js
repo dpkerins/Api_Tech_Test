c0aaaf4c8dc820f31d2eb715fa8f07dd530219ad
Douglas <douglaskerins@gmail.com>
2022-03-07 11:07:04 +0000

    adding name duplicate validation

--- routes/__tests__/players.test.js
+++ routes/__tests__/players.test.js
@@ -1,26 +1,39 @@
 const app = require('../../app.js');
 const request = require('supertest');
+const { PrismaClient, Prisma } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 describe("/new", () => {
+  beforeEach(async () => {
+    await prisma.player.deleteMany({})
+  })
+  const body = {
+    first_name: 'New',
+    last_name: 'Guy',
+    nationality: 'Germany',
+    dob: 642726000
+  }
   it("should add a new player to the database", async () => {
-    const body = {
-      first_name: 'New',
-      last_name: 'Guy',
-      nationality: 'Germany',
-      dob: 642726000
-    }
     const response = await request(app)
       .post('/players/new')
       .send(body)
       .set('Accept', 'application/json')
-    expect(response.body).toEqual({
-      id: 1,
-      first_name: 'New',
-      last_name: 'Guy',
-      nationality: 'Germany',
-      dob: 642726000,
-      score: 1200
-    })
+    expect(response.body.first_name).toEqual("New");
+    expect(response.body.last_name).toEqual("Guy");
+    expect(response.body.score).toEqual(1200);
+    expect(response.body.nationality).toEqual("Germany");
+  })
+
+  it("should not allow duplicate player (same first and last name)", async () => {
+    const response = await request(app)
+      .post('/players/new')
+      .send(body)
+      .set('Accept', 'application/json')
+    const responseDuplicate = await request(app)
+      .post('/players/new')
+      .send(body)
+      .set('Accept', 'application/json')
+    expect(responseDuplicate.body).toEqual("Cannot enter a new player with the same first and last names of an existing player");
   })
 })
 
--- routes/players.js
+++ routes/players.js
@@ -10,17 +10,29 @@ router.get('/', function(req, res, next) {
 
 router.post('/new', async (req, res) => {
   const body = req.body;
-  const data = {
-    first_name: body.first_name,
-    last_name: body.last_name,
-    nationality: body.nationality,
-    dob: body.dob,
-    score: 1200
+  const user = await prisma.player.findFirst({
+    where: { first_name: body.first_name, last_name: body.last_name },
+  })
+  try {
+    if (user == null) {
+      const data = {
+        first_name: body.first_name,
+        last_name: body.last_name,
+        nationality: body.nationality,
+        dob: body.dob,
+        score: 1200
+      }
+      const newPlayer = await prisma.player.create({
+        data: data
+      });
+      res.json(newPlayer);
+    } else {
+      throw ("Cannot enter a new player with the same first and last names of an existing player");
+    }
+  } catch (e) {
+    res.json(e);
   }
-  const newPlayer = await prisma.player.create({
-    data: data
-  });
-  res.json(newPlayer);
+  
 })
 
 module.exports = router;
