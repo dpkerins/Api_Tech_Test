3b972b64f349e193ed795cc7f608e3548e3b39ac
Douglas <douglaskerins@gmail.com>
2022-03-08 17:48:58 +0000

    changing parseInt to Floor for to calcnewscores

--- calcNewScores.js
+++ calcNewScores.js
@@ -1,5 +1,5 @@
 const calcNewScores = (winner, loser) => {
-  const transfer = parseInt(loser.score / 10);
+  const transfer = Math.floor(loser.score / 10);
   return {
     winnerPoints: winner.score + transfer,
     loserPoints: loser.score - transfer
